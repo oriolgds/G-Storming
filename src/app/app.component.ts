7dadb23440970c637c9b5331e88784f19e7180ac
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-06-05 07:39:53 +0000

    Trying to add another component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'; 
+import {MatTabsModule} from '@angular/material/tabs';
+
 
 @Component({
-  selector: 'loader',
+  selector: 'app-loader',
   standalone: true,
   imports: [RouterOutlet, MatProgressSpinnerModule],
   templateUrl: './loader/loader.html',
-  styleUrl: './loader/loader.css'
+  styleUrl: './loader/loader.css',
 })
+
+@Component({
+  selector: 'main-tabs',
+  standalone: true,
+  imports: [RouterOutlet, MatTabsModule],
+  templateUrl: './mainTabs/mainTabs.html',
+  styleUrl: './mainTabs/mainTabs.css'
+})
+
 export class AppComponent {
   title = 'G-Storming';
 }
